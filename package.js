f98c68e62470749011314dce77ebee4fe309fb1f
Peter Pavlovich <pavlovich@gmail.com>
2014-06-18 00:27:17 -0400

    Switch to stylus processing rather than less.

--- package.js
+++ package.js
@@ -5,6 +5,7 @@ Package.describe({
 Package.on_use(function (api) {
 
     api.use('templating', 'client');
+    api.use('stylus', 'client');
     api.use('underscore-string-latest', 'client');
     api.use('ngMeteor', ['client']);
     api.use('meteor-fleximodel', ['client', 'server']);
@@ -12,7 +13,7 @@ Package.on_use(function (api) {
 
     // Files to load in Client only.
     api.add_files([
-        'styles/styles.less',
+        'styles/styles.styl',
         'templates/sgi_div.html',
         'templates/sgi_form.html',
         'templates/sgi_field.html',
